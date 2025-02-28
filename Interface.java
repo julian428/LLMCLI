d9d975d3569495384bcd621351674736c0fd4f0a
Julian <julianjometzger@gmail.com>
2025-02-28 08:23:40 +0100

    fix: Removed sensitive data

--- Interface.java
+++ Interface.java
@@ -1,9 +1,28 @@
+import java.util.Scanner;
+
 public class Interface{
 	public static void main(String[] args){
 		System.out.println("Welcome to dumGPT");
-		Api api = new Api("qwen:0.5b", "http://localhost:11434");
+		
+		clear();
+		while(loop());
+	}
 
-		String response = api.generateResponse("What is your name?");
-		System.out.println(response);
+	private static boolean loop(){
+		Scanner input = new Scanner(System.in);
+		System.out.print("prompt: ");
+		String prompt = input.nextLine();
+
+		if(prompt.equals("!q")) return false;
+
+		Api api = new Api("qwen:0.5b", "http://localhost:11434");
+		String response = api.generateResponse(prompt);
+		System.out.println("response: " + response);
+		return true;
+	}
+
+	private static void clear(){
+		System.out.print("\033[H\033[2J");
+		System.out.flush();
 	}
 }
